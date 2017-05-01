44b2b63008165610755a946f439bdf0b64dc5177
Stephane Moreau <st.moreau@outlook.com.gr>
2017-05-01 01:54:16 +0100

    clean up && add prebuild && webpack-config-utils

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,15 +1,17 @@
+const {resolve} = require('path')
 const webpackValidator = require('webpack-validator')
-const { resolve } = require('path')
+const {getIfUtils} = require('webpack-config-utils')
 
 module.exports = env => {
+  const {ifProd} = getIfUtils(env)
   return webpackValidator({
     context: resolve('src'),
     entry: './bootstrap.js',
     output: {
-      path: resolve('dist'),
       filename: 'bundle.js',
+      path: resolve('dist'),
       publicPath: '/dist/',
     },
-    devtool: env.prod ? 'source-map' : 'eval',
+    devtool: ifProd('source-map', 'eval'),
   })
 }
