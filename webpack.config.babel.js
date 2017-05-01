8963e695bfd156613b76d0485f2e2b37b481c13e
Stephane Moreau <st.moreau@outlook.com.gr>
2017-05-01 01:05:06 +0100

    webpack validator to have clearer error messages

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,11 +1,12 @@
+const webpackValidator = require('webpack-validator')
 const { resolve } = require('path')
 
 module.exports = () => {
-  return {
+  return webpackValidator({
     context: resolve('src'),
     entry: './bootstrap.js',
     output: {
       filename: 'bundle.js',
     },
-  }
+  })
 }
